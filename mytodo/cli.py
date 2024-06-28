6bbfa751ef59e3ad46b791e8b718c2f0c19b8971
Anirudha Kanakatte <anirudha@kanakatte.com>
2024-06-28 23:15:58 +0100

    Add init command

--- mytodo/cli.py
+++ mytodo/cli.py
@@ -1,11 +1,38 @@
 """ This module provides the CLI. """
-
+from pathlib import Path
 from typing import Optional
 import typer
 
-from mytodo import __app_name__, __version__
+from mytodo import ERRORS, __app_name__, __version__, config, database
 
 app = typer.Typer()
+
+@app.command()
+def init(
+    db_path: str = typer.Option(
+        str(database.DEFAULT_DB_FILE_PATH),
+        "--db-path",
+        "-db",
+        prompt="todo database location?",
+    )
+) -> None:
+    """ initialize the todo database """
+    app_init_error = config.init_app(db_path=db_path)
+    if app_init_error:
+        typer.secho(
+            f'Creating config file failed with "{ERRORS[app_init_error]}"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    db_init_error = database.init_database(db_path=Path(db_path))
+    if db_init_error:
+        typer.secho(
+            f'Creating database failed with "{ERRORS[db_init_error]}"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    else:
+        typer.secho(f"The todo database is {db_path}", fg=typer.colors.GREEN)
 
 def _version_callback(value: bool) -> None:
     if value:
